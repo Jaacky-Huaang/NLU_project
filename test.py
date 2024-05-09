a7d809e27529681696486df61fee5aa415c2515b
Jaacky-Huaang <104845011+Jaacky-Huaang@users.noreply.github.com>
2024-05-09 11:53:08 -0400

    Update test.py

--- test.py
+++ test.py
@@ -12,6 +12,10 @@ from datasets import load_metric
 
 from train import preprocess_dataset
 
+import torch 
+import argparse
+from collections.abc import Iterable
+
 def compute_metrics(eval_pred):
     accuracy_metric = load_metric("accuracy")
     logits, labels = eval_pred
@@ -39,7 +43,6 @@ def init_tester(directory: str) -> Trainer:
     report_to="all",
     per_device_eval_batch_size=8,
     evaluation_strategy="no",
-
     )
 
     tester = Trainer(
@@ -50,26 +53,63 @@ def init_tester(directory: str) -> Trainer:
     return tester
 
 
-if __name__ == "__main__":  # Use this script to test your model
-    model_name = "prajjwal1/bert-tiny"
+# Set up argument parser
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train and test the model.")
 
-    # Load IMDb dataset
-    imdb = load_dataset("imdb")
-    del imdb["train"]
-    del imdb["unsupervised"]
+    # Important arguments
+    parser.add_argument("--dataset_name", type=str, default="wnli", choices=[ "qnli", "rte", "sst2",  "wnli"], help="The name of the dataset to use.")
+    parser.add_argument("--non_bitfit_layers", type=int, default=0, choices=[0, 1, 2, 3, 4], help="The number of non-bitfit layers to use.")
+    
+    # Some of these might be useless
+    parser.add_argument("--num_labels", type=int, default=2) # Actually all are 2
+    parser.add_argument("--model_name", type=str, default="prajjwal1/bert-mini")
+    parser.add_argument("--tokenizer_name", type=str, default="prajjwal1/bert-mini")
+    parser.add_argument("--num_trials", type=int, default=5,
+                        help="The number of hyperparameter tuning trials to run.")
+    parser.add_argument("--num_epochs", type=int, default=8,
+                        help="The number of epochs to train the model for.")
+    parser.add_argument("--batch_size", type=int, default=8,
+                        help="The batch size to use for training.")
+    parser.add_argument("--learning_rate", type=float, default=3e-4,
+                        help="The learning rate to use for training.")
+    parser.add_argument("--seed", type=int, default=42,
+                        help="The random seed to use for training.")
+    return parser.parse_args()
 
-    # Preprocess the dataset for the tester
-    tokenizer = BertTokenizerFast.from_pretrained(model_name)
-    imdb["test"] = preprocess_dataset("sst2", imdb["test"], tokenizer)
 
-    # Set up tester
-    tester = init_tester("/scratch/jh7956/NLU/project/checkpoints/checkpoint-8419")
+def main(args):
 
-    # Test
-    results = tester.predict(imdb["test"])
-    with open("test_results_without_bitfit.p", "wb") as f:
-        pickle.dump(results, f)
+    # Load the tokenizer
+    tokenizer = BertTokenizerFast.from_pretrained(args.tokenizer_name)
+    print("Tokenizer loaded")
 
-    with open("test_results_without_bitfit.p", "rb") as f:
-        data = pickle.load(f)
-    print(data)
+    # Prepare the GLUE dataset from huggingface
+    dataset = load_dataset("glue", args.dataset_name)
+    split = dataset["train"].train_test_split(test_size=0.2)
+    dataset["test"] = dataset["validation"]
+    dataset["test"] = preprocess_dataset(args.dataset_name, dataset["test"], tokenizer)
+    print("Dataset loaded")
+
+    test_files = ["/scratch/jh7956/NLU_project/checkpoints/run-0/checkpoint-64", 
+                "/scratch/jh7956/NLU_project/checkpoints/run-1/checkpoint-32",
+                "/scratch/jh7956/NLU_project/checkpoints/run-2/checkpoint-32",
+                "/scratch/jh7956/NLU_project/checkpoints/run-3/checkpoint-256",
+                "/scratch/jh7956/NLU_project/checkpoints/run-4/checkpoint-64",
+                "/scratch/jh7956/NLU_project/checkpoints/run-5/checkpoint-256",
+                "/scratch/jh7956/NLU_project/checkpoints/run-7/checkpoint-128",]
+
+    for i in range(len(test_files)):
+        file = test_files[i]
+        tester = init_tester(file)
+        results = tester.predict(dataset["test"])
+        # with open(f"test_results_{file}.p", "wb") as f:
+        #     pickle.dump(results, f)
+
+        # with open(f"test_results_{file}.p", "rb") as f:
+        #     data = pickle.load(f)
+        print(results)
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
